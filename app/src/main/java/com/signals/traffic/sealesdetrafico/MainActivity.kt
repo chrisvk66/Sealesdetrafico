57c8abb1e86b5716c754190c6e70b6fb5e61b2c6
chris <chrisvktorrejon@gmail.com>
2017-07-19 19:27:47 +0200

    Añadido primer intent.

--- app/src/main/java/com/signals/traffic/sealesdetrafico/MainActivity.kt
+++ app/src/main/java/com/signals/traffic/sealesdetrafico/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.signals.traffic.sealesdetrafico
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun activitychange(view: View){
+
+        val i: Intent = Intent(this, Signals::class.java)
+
+        startActivity(i)
     }
 }
