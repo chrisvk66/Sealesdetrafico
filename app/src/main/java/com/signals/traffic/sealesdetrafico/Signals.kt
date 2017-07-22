15677a678db52ed6e4c45527550003539004f6a3
david2319 <david.martin.molina@gmail.com>
2017-07-22 17:24:14 +0200

    "Peligro" completado

--- app/src/main/java/com/signals/traffic/sealesdetrafico/Signals.kt
+++ app/src/main/java/com/signals/traffic/sealesdetrafico/Signals.kt
@@ -12,7 +12,11 @@ class Signals : AppCompatActivity() {
         setContentView(R.layout.signals)
     }
 
-    fun priority(view:View){
+    fun danger(view:View){
         startActivity(Intent(this, Signals_danger::class.java))
+    }
+
+    fun priority(view:View){
+        startActivity(Intent(this, Signals_priority::class.java))
     }
 }
